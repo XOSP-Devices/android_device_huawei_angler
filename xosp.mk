dcfce9d6ce2b911f200df9f28bdbd7af91fb90b1
Nicholas Pace <nicholaspace97@yahoo.ca>
2017-05-05 23:05:57 -0400

    Update and rename lineage.mk to xosp.mk
    changes for xosp

--- xosp.mk
+++ xosp.mk
@@ -2,18 +2,26 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common XOSP stuff.
 
-# Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/angler/overlay-cm
+
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_angler
+PRODUCT_NAME := xosp_angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
 
+IS_ARM64 := TRUE
 TARGET_VENDOR := huawei
+RELEASE_TYPE := XOSP_OFFICIAL
+TARGET_VENDOR := huawei
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
